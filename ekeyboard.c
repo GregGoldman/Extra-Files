82c3313be28cfc667153302c8699a95c03775f9c
GregGoldman <ghg3@duke.edu>
2016-08-08 00:11:21 -0400

    work for the ekeyboard mex file

--- ekeyboard.c
+++ ekeyboard.c
@@ -7,7 +7,7 @@
  
 //Compile via:
 mex ekeyboard.c
-  is this required to be part of the startup file?
+ must have a MinGW-w64 C/C++ compiler installed!
  */
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
@@ -16,7 +16,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
     //TODO: Run a mexcallmatlab line which calls a helper function => ekeyboard_helper (place in its own .m file)
     //that initializes and runs the timer
     //http://www.mathworks.com/help/matlab/apiref/mexcallmatlab.html
-    mexCallMATLAB(0,NULL,0,NULL,'ekeyboard_helper')
+    mexCallMATLAB(0,NULL,0,NULL,"ekeyboard_helper");
     // this will need arguments, specifically at last a calling_function_info(??)
     
     //This works, unlike evalin('caller','keyboard') in a .m file
